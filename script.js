96d226c9283ab962c64bdec893992b2713385d12
Azhwake <azhwaq2002@gmail.com>
2024-09-21 22:40:52 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Good Luck 💪🏾
+function pluralize(words) {
+    const wordCount = {};
+
+    // Count occurrences of each word
+    words.forEach(word => {
+        wordCount[word] = (wordCount[word] || 0) + 1;
+    });
+
+    // Convert to plural form if count > 1
+    return Object.keys(wordCount).map(word => {
+        return wordCount[word] > 1 ? word + 's' : word;
+    });
+}
+
+// Test cases
+console.log(pluralize(["cat", "dog", "cat", "mouse"])); // ➞ ["cats", "dog", "mouse"]
+console.log(pluralize(["car", "car", "car"]));         // ➞ ["cars"]
